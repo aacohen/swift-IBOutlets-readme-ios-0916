996c91d6eeba323cbeca13ea96b9e9e795c6c926
aacohen <arielaades@gmail.com>
2016-09-21 12:33:29 -0400

    Swift IBOutlets complete

--- ViewController.swift
+++ ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textDisplay: UILabel!
+    
+   
+    @IBAction func helloWorld(_ sender: AnyObject) {
+        textDisplay.text = "Hello, world!"
+        print("Pressed Hello World Button")
+    }
+    
+    @IBAction func goodbyeWorld(_ sender: AnyObject) {
+        textDisplay.text = "Goodbye, world!"
+        print("Pressed Goodbye World Button")
+    }
+    
+    @IBAction func Unicorn(_ sender: AnyObject) {
+        textDisplay.text = "🦄"
+        print("Pressed Unicorn Button")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
